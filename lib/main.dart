c6afa28df47637998c5729c04f47b98e3e16c02b
Maalug Jonathan <jmaalug11@gmail.com>
2024-08-21 15:48:29 +0000

    added persistent storage for the dark theme or theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'screens/splash.dart';
 import 'values/app_props.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  try {
-    if (kIsWeb) {
-      // Initialize Hive for web
-      Hive.init('');
-    } else {
-      // Initialize Hive for mobile
-      final appDocumentDir = await getApplicationDocumentsDirectory();
-      Hive.init(appDocumentDir.path);
-    }
-    Box box = await Hive.openBox("iprisinfo");
-    runApp(MyApp(box: box));
-  } catch (e) {
-    print('Error initializing Hive: $e');
-  }
-}
 
 class MyApp extends StatefulWidget {
   final Box box;
@@ -37,37 +17,54 @@ class _MyAppState extends State<MyApp> {
   Map appProps = AppProps().myAppProps;
   ThemeMode _themeMode = ThemeMode.light;
 
-  void _changeTheme(ThemeMode themeMode) {
-    setState(() {
-      _themeMode = themeMode;
-    });
-  }
-
   @override
   void initState() {
     super.initState();
+    _loadTheme();
     storeInitialData();
+  }
+
+  Future<void> _loadTheme() async {
+    try {
+      SharedPreferences sharedPreferences =
+          await SharedPreferences.getInstance();
+      bool isDarkTheme = sharedPreferences.getBool('isDarkTheme') ?? false;
+      setState(() {
+        _themeMode = isDarkTheme ? ThemeMode.dark : ThemeMode.light;
+      });
+    } catch (e) {
+      print('Error loading theme from SharedPreferences: $e');
+    }
   }
 
   Future<void> storeInitialData() async {
     try {
-      print("Attempting to initialize SharedPreferences");
       SharedPreferences sharedPreferences =
           await SharedPreferences.getInstance();
-      print("SharedPreferences initialized successfully");
-
       var box = widget.box;
+
       await box.put('appProps', appProps);
-      if (!box.containsKey('firstRun')) {
+      if (!sharedPreferences.containsKey('firstRun')) {
         await box.put('firstRun', false);
         await box.put('isLoggedIn', false);
         sharedPreferences.setBool('firstRun', false);
         sharedPreferences.setBool('isLoggedIn', false);
-        print("Done... with initializing");
+        sharedPreferences.setBool('isDarkTheme', false);
       }
     } catch (e) {
       print('Error initializing SharedPreferences: $e');
     }
+  }
+
+  void _changeTheme(ThemeMode themeMode) async {
+    setState(() {
+      _themeMode = themeMode;
+    });
+
+    // Save the theme mode to SharedPreferences
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    bool isDarkTheme = (themeMode == ThemeMode.dark);
+    sharedPreferences.setBool('isDarkTheme', isDarkTheme);
   }
 
   @override
